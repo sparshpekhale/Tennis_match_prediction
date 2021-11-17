f3f45c11e432e6d7c3db9c468f34e1017ac55b2f
sparshpekhale <sparsh8910@yahoo.com>
2021-11-17 16:38:05 +0530

    hyperparameter tuning

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
+import numpy as np
 from itertools import tee
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, plot_roc_curve, roc_curve, roc_auc_score
 
 import matplotlib.pyplot as plt
 
-def make_ds(data, train_split=0.3, shuffle=False):
-    X = data.drop(['player0_id', 'player0_name', 'player1_id', 'player1_name', 'winner'], axis=1).to_numpy()
+def make_ds(data, train_split=0.7, shuffle=False):
+    X = data.drop(['player0_rank', 'player1_rank', 'player0_id', 'player0_name', 'player1_id', 'player1_name', 'round', 'minutes', 'winner'], axis=1).to_numpy()
     y = data['winner'].to_numpy()
+
+    if train_split==1:
+        return [X, np.array([]), y, np.array([])]
+    if train_split==0:
+        return [np.array([]), X, np.array([]), y]
 
     return train_test_split(X, y, train_size=train_split, shuffle=shuffle)
 
@@ -14,7 +20,6 @@ def train_models(dataset, models):
     X_train, X_test, y_train, y_test = dataset
 
     trained = []
-    preds = []
     for model in models:
         print(f'Training model -> {model}')
         model.fit(X_train, y_train)
@@ -22,10 +27,32 @@ def train_models(dataset, models):
         print(classification_report(y_test, model.predict(X_test)))
 
         trained.append(model)
-        preds.append(model.predict(X_test))
         print('='*80)
-    return trained, preds
+    return trained
 
+def get_preds(dataset, models):
+    X_train, X_test, y_train, y_test = dataset
+
+    preds = []
+    for model in models:
+        preds.append(model.predict(X_test))
+    return preds
+
+def get_probas(dataset, models):
+    X_train, X_test, y_train, y_test = dataset
+
+    probas = []
+    for model in models:
+        probas.append(model.predict_proba(X_test).max(axis=1))
+    return np.array(probas)
+
+def hard_voting(preds):
+    preds = np.array(preds)
+    return (preds.sum(axis=0)>len(preds)/2)*1
+
+def soft_voting(probas):
+    preds = np.array(probas)
+    return (preds.mean(axis=0)>0.5)*1
 
 def plot_rocs(dataset, models, preds):
     X_train, X_test, y_train, y_test = dataset
@@ -42,8 +69,24 @@ def plot_rocs(dataset, models, preds):
     # plt.legend(loc="lower right")
     # plt.show()
 
-
+    plt.figure()
     fig = plot_roc_curve(models[0], X_test, y_test)
     for model in models[1:]:
         fig = plot_roc_curve(model, X_test, y_test, ax = fig.ax_)
+    plt.show()
+    return fig
+
+def plot_bar(title, models, acc, labels, nn_acc=None):
+    if nn_acc:
+        plt.bar(
+            [model.__str__().split('(')[0] for model in models]+labels,
+            np.hstack([acc, nn_acc])
+        )
+    else:
+        plt.bar(
+            [model.__str__().split('(')[0] for model in models],
+            acc
+        )
+    plt.xticks(rotation='vertical')
+    plt.title(title)
     plt.show()
--- preprocessing.py
+++ preprocessing.py
@@ -3,9 +3,6 @@ import random
 
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-def test(st):
-    return st+'1'
-
 def data_preprocess(data):
 
     # drop unwanted features
@@ -29,15 +26,25 @@ def data_preprocess(data):
         ren_col.append(col.replace('winner_', 'player0_').replace('loser_', 'player1_').replace('w_', 'p0_').replace('l_', 'p1_'))
     data.columns = ren_col
 
+    # Drop more features
+    drop = ['p0_ace', 'p0_df', 'p0_svpt', 'p0_1stIn', 'p0_1stWon', 'p0_2ndWon',
+       'p0_SvGms', 'p0_bpSaved', 'p0_bpFaced', 'p1_ace', 'p1_df', 'p1_svpt',
+       'p1_1stIn', 'p1_1stWon', 'p1_2ndWon', 'p1_SvGms', 'p1_bpSaved',
+       'p1_bpFaced']
+    data.drop(drop, axis=1, inplace=True)
+
     # Add 'winner' column
     data['winner'] = 0
 
+    # print(data.iloc[0, :])
     # Switch some winners and losers to obtain winner '0's for model to train
     for i in range(data.shape[0]):
         if random.random() > 0.5:
             data.iloc[i, 3:11], data.iloc[i, 11:19] = data.iloc[i, 11:19], data.iloc[i, 3:11]
-            data.iloc[i, 22:31], data.iloc[i, 31:40] = data.iloc[i, 31:40], data.iloc[i, 22:31]
+            # data.iloc[i, 22:31], data.iloc[i, 31:40] = data.iloc[i, 31:40], data.iloc[i, 22:31]
             data.iloc[i, -1] = 1
+    # print(data.iloc[0, :])
+    
     
     # Find floats to standardize
     floats = []
